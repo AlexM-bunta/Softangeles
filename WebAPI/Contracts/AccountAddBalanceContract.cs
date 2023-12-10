af3c7a32514022b11e787c853b68b97b267003c2
AlexM-bunta <genotheblast23@gmail.com>
2023-12-10 10:44:18 +0200

    asd

--- WebAPI/Contracts/AccountAddBalanceContract.cs
+++ WebAPI/Contracts/AccountAddBalanceContract.cs
@@ -4,5 +4,5 @@ public class AccountAddBalanceContract
 {
     public Guid SessionId { get; set; }
     public decimal Amount { get; set; }
-    public string IBAN { get; set; }
+    public string IBAN { get; set; } = string.Empty;
 }
